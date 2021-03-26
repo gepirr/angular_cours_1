f092539a7763e4ed8b11b480f868fdc4ff61a709
gepirr <ynx.melody@icloud.com>
2021-03-27 00:51:49 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -13,7 +13,7 @@ import { Component, EventEmitter, Input,Output } from '@angular/core';
       {{ getValue(value) }}
     </h1>
   `,
-  //          [(ngModel)]="value"
+  //      [(ngModel)]="value"  type="range"(拖拽条) checkbox选择器
   //ngModelChange,意思是balise input et mis en evenement et je mis abonne
   //$event, event est une variable que ngmodelchange a produit (如果input的type是text则event的type也是text)
   styles: [`h1 { font-family: Lato; }`]
@@ -26,6 +26,7 @@ export class HelloComponent  {
   //lorsque je vais definir ma balise hello, je vais pouvoir lui passer un attribut name
   //链接此class中的name和html中的name
   @Output() monEvt = new EventEmitter<number>();
+
   getValue(v: number): number {
     return Math.round(v/10);
   }
